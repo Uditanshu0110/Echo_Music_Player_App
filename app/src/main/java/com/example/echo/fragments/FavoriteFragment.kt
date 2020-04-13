a12c204e3e07a73764b1966ba1b5b6d4a22d8684
uditanshu pandey <uditanshupandey00@gmail.com>
2020-04-13 19:34:54 +0530

    Completed.

---
+++ app/src/main/java/com/example/echo/fragments/FavoriteFragment.kt
@@ -0,0 +1,221 @@
+package com.example.echo.fragments
+
+
+import android.app.Activity
+import android.content.Context
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.RelativeLayout
+import android.widget.TextView
+import com.example.echo.R
+import com.example.echo.Songs
+import com.example.echo.adapters.FavoriteAdapter
+import com.example.echo.databases.EchoDatabase
+import com.example.echo.fragments.SongPlayingFragment.Statified.favoriteContent
+import com.example.echo.fragments.SongPlayingFragment.Statified.myActivity
+import kotlinx.android.synthetic.main.fragment_favorite.*
+
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class FavoriteFragment : Fragment() {
+    var myActivity: Activity? = null
+    var noFavorites: TextView? = null
+    var nowPlayingBottomBar: RelativeLayout? = null
+    var playPauseButton: ImageButton? = null
+    var songTitle: TextView? = null
+    var recyclreView: RecyclerView? = null
+    var trackPosition: Int = 0
+    var favoriteContent: EchoDatabase? = null
+    var reFreshList: ArrayList<Songs>? = null
+    var getListfromDatabase: ArrayList<Songs>? = null
+
+
+    object Statified {
+        var mediaPlayer: MediaPlayer? = null
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        activity?.title = "Favorites"
+        val view = inflater!!.inflate(R.layout.fragment_favorite, container, false)
+        noFavorites = view?.findViewById(R.id.noFavorites)
+        nowPlayingBottomBar = view.findViewById(R.id.hiddenBarFavScreen)
+        songTitle = view.findViewById(R.id.songTitle)
+        playPauseButton = view.findViewById(R.id.playPauseButton)
+        recyclreView = view.findViewById(R.id.favoriteRecycler)
+        return view
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        myActivity = context as Activity
+    }
+
+    override fun onAttach(activity: Activity?) {
+        super.onAttach(activity)
+        myActivity = activity
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        display_favorites_by_searching()
+        bottomBarSetup()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?) {
+        super.onPrepareOptionsMenu(menu)
+        val item = menu?.findItem(R.id.action_sort)
+        item?.isVisible = false
+    }
+
+    fun getSongsFromPhone(): ArrayList<Songs>? {
+        var arrayList = ArrayList<Songs>()
+        var contentResolver = myActivity?.contentResolver
+        var songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        var songCursor = contentResolver?.query(songUri, null, null, null, null)
+
+        if (songCursor != null && songCursor.moveToFirst()) {
+            val songId = songCursor.getColumnIndex(MediaStore.Audio.Media._ID)
+            val songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE)
+            val songArtist = songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST)
+            val songData = songCursor.getColumnIndex(MediaStore.Audio.Media.DATA)
+            val dateIndex = songCursor.getColumnIndex(MediaStore.Audio.Media.DATE_ADDED)
+
+            while (songCursor.moveToNext()) {
+                var currentId = songCursor.getLong(songId)
+                var currentTitle = songCursor.getString(songTitle)
+                var currentArtist = songCursor.getString(songArtist)
+                var currentData = songCursor.getString(songData)
+                var currentDate = songCursor.getLong(dateIndex)
+                arrayList.add(Songs(currentId, currentTitle, currentArtist, currentData, currentDate))
+            }
+
+        } else {
+            return null
+        }
+        return arrayList
+    }
+
+    fun bottomBarSetup() {
+        try {
+            bottomBarClickHandler()
+            songTitle?.setText(SongPlayingFragment.Statified.currentSongHelper?.songTitle)
+            SongPlayingFragment.Statified.mediaPlayer?.setOnCompletionListener({
+                songTitle?.setText(SongPlayingFragment.Statified.currentSongHelper?.songTitle)
+                SongPlayingFragment.Staticated.onSongComplete()
+            })
+            if (SongPlayingFragment.Statified.mediaPlayer?.isPlaying as Boolean) {
+                nowPlayingBottomBar?.visibility = View.VISIBLE
+            } else {
+                nowPlayingBottomBar?.visibility = View.INVISIBLE
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun bottomBarClickHandler() {
+
+        nowPlayingBottomBar?.setOnClickListener({
+            Statified.mediaPlayer = SongPlayingFragment.Statified.mediaPlayer
+            val songPlayingFragment = SongPlayingFragment()
+            val args = Bundle()
+
+            args.putString("songArtist", SongPlayingFragment.Statified.currentSongHelper?.songArtist)
+            args.putString("path", SongPlayingFragment.Statified.currentSongHelper?.songPath)
+            args.putString("songTitle", SongPlayingFragment.Statified.currentSongHelper?.songTitle)
+            args.putInt("SongId", SongPlayingFragment.Statified.currentSongHelper?.songId?.toInt() as Int)
+            args.putInt(
+                "songPosition",
+                SongPlayingFragment.Statified.currentSongHelper?.currentPosition?.toInt() as Int
+            )
+            args.putParcelableArrayList("songData", SongPlayingFragment.Statified.fetchSongs)
+            args.putString("FavBottomBar", "success")
+            songPlayingFragment.arguments = args
+            fragmentManager?.beginTransaction()
+                ?.replace(R.id.details_fragment, songPlayingFragment)
+                ?.addToBackStack("SongPlayingFraagment")
+                ?.commit()
+        })
+        playPauseButton?.setOnClickListener({
+            if (SongPlayingFragment.Statified.mediaPlayer?.isPlaying as Boolean) {
+                SongPlayingFragment.Statified.mediaPlayer?.pause()
+                trackPosition = SongPlayingFragment.Statified.mediaPlayer?.getCurrentPosition() as Int
+                playPauseButton?.setBackgroundResource(R.drawable.play_icon)
+            } else {
+                SongPlayingFragment.Statified.mediaPlayer?.seekTo(trackPosition)
+                SongPlayingFragment.Statified.mediaPlayer?.start()
+                playPauseButton?.setBackgroundResource(R.drawable.pause_icon)
+            }
+        })
+    }
+
+    fun display_favorites_by_searching() {
+
+        if (favoriteContent?.checkSize() as Int > 0) {
+            reFreshList = ArrayList<Songs>()
+            getListfromDatabase = favoriteContent?.queryDBList()
+            val fetchListfromDevice = getSongsFromPhone()
+            if (fetchListfromDevice != null) {
+                for (i in 0..fetchListfromDevice?.size - 1) {
+                    for (j in 0..getListfromDatabase?.size as Int - 1) {
+                        if ((getListfromDatabase?.get(j)?.songId === (fetchListfromDevice?.get(i)?.songId))) {
+                            reFreshList?.add((getListfromDatabase as ArrayList<Songs>)[j])
+                        }
+                    }
+                }
+
+            } else {
+
+            }
+
+
+            if (reFreshList == null) {
+                recyclreView?.visibility = View.INVISIBLE
+                noFavorites?.visibility = View.VISIBLE
+            } else {
+                var favoriteAdapter = FavoriteAdapter(
+                    reFreshList as ArrayList<Songs>,
+                    SongPlayingFragment.Statified.myActivity as Context
+                )
+                val mLayoutManager = LinearLayoutManager(activity)
+                recyclreView?.layoutManager = mLayoutManager
+                recyclreView?.itemAnimator = DefaultItemAnimator()
+                recyclreView?.adapter = favoriteAdapter
+                recyclreView?.setHasFixedSize(true)
+
+            }
+        } else {
+            recyclreView?.visibility = View.INVISIBLE
+            noFavorites?.visibility = View.VISIBLE
+        }
+
+    }
+}
